6b36703542526f1d0bc942a240bf57a90365028a
Yogesh <yj981796@gmail.com>
2025-12-19 01:30:42 +0530

    Deploy using docs folder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-   base: '/tailwind_project/',
+   base: '/Tailwind_project/',
   plugins: [react(),  tailwindcss()],
 })
